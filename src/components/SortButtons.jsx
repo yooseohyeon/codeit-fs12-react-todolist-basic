818bb96a69bd1355d005dc680ec7e20e08587353
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-24 13:13:46 +0900

    feat: 할일 정렬 기능 구현 (최신순/오래된순)

--- src/components/SortButtons.jsx
+++ src/components/SortButtons.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const SortButtons = ({ setTodos }) => {
-  return <div>SortButtons</div>;
+  const [sortBy, setSortBy] = useState("latest");
+
+  const handelSortChange = (value) => {
+    setSortBy(value);
+
+    if (value === "oldest") {
+      setTodos((prev) =>
+        [...prev].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)),
+      );
+    } else {
+      setTodos((prev) =>
+        [...prev].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)),
+      );
+    }
+  };
+
+  return (
+    <select value={sortBy} onChange={(e) => handelSortChange(e.target.value)}>
+      <option value="latest">최신순</option>
+      <option value="oldest">오래된순</option>
+    </select>
+  );
 };
