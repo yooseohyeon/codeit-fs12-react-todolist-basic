a53a65d0c62ca6cc696fdce103507b4dd5c5eae2
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-24 10:24:55 +0900

    feat: json sever 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
 import { SortButtons } from "./components/SortButtons";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "리액트 공부하기",
-      completed: false,
-      createdAt: "2026-03-18T12:00:00.000Z",
-    },
-    {
-      id: 2,
-      title: "장보기",
-      completed: true,
-      createdAt: "2026-03-18T12:00:00.000Z",
-    },
-    {
-      id: 3,
-      title: "운동하기",
-      completed: false,
-      createdAt: "2026-03-18T12:00:00.000Z",
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const res = await fetch("http://localhost:4000/todos", {
+        headers: {
+          Accept: "application/json",
+        },
+        method: "GET",
+      });
+      const data = await res.json();
+
+      console.log(data);
+      setTodos(data);
+    };
+    getTodos();
+  }, []);
+
   return (
     <>
       <h1>Todo List</h1>
-      <TodoForm setTodos={setTodos} />
+      <TodoForm todos={todos} setTodos={setTodos} />
       <SortButtons setTodos={setTodos} />
       <TodoList setTodos={setTodos} />
     </>
